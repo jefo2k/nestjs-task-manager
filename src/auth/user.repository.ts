b97afe71dab0a06a5fabac9e9ad62e6baef12505
Jeferson Santos <jefo2k@gmail.com>
2020-07-28 20:38:38 -0300

    add signup

---
+++ src/auth/user.repository.ts
@@ -0,0 +1,26 @@
+import { Repository, EntityRepository } from 'typeorm';
+import { User } from './user.entity';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import { ConflictException, InternalServerErrorException } from '@nestjs/common';
+
+@EntityRepository(User)
+export class UserRepository extends Repository<User> {
+  async signup(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    const { username, password } = authCredentialsDto;
+
+    const user = new User();
+    user.userName = username;
+    user.password = password;
+
+    try {
+      await user.save();      
+    } catch (error) {
+      if (error.code === '23505') { // duplicated username
+        throw new ConflictException('username already exists');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
+
+  }
+}
