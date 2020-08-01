0988283285308e4e9d4b88379a05f75036fa3972
Jeferson Santos <jefo2k@gmail.com>
2020-08-01 00:11:29 -0300

    remove query logging

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -9,5 +9,5 @@ export const typeOrmConfig: TypeOrmModuleOptions = {
   database: 'taskmanager',
   entities: [__dirname + '/../**/*.entity.{js,ts}'],
   synchronize: true,
-  logging: ["query", "error"]
+  logging: ["error"] // "query", "error"
 }
